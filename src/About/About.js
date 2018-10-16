62fa73bb3fc891816aa7f65f2f2cca4a51cfb588
David <david_zetterdahl@hotmail.com>
2018-10-16 17:52:35 +0200

    Update About.js

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 
 const aboutText = {id: "i9", name: "David Zetterdahl", baseGame: "''LordDz''", type: "", url: "email:david_zetterdahl@hotmail.com", videoUrl: "amRoh96Z2to", classNames: "bg-about",
-text: "A game developer with a serious passion for games. I've been making games ever since my older brother bought me Starcraft and it has continued from there. Below you can see some of the mods and games I've created in the last few years."
-    + "\nI'm currently 27 years old and living in Gothenburg, Sweden.",
+text: "Game developer with a passion for creating games.",
 notes: "The site is made from scratch with React."
 };
 
